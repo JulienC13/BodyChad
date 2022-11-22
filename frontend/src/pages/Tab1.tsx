19a78867cb0258bf359cdd3871a68b74b1a3b1af
Julien Charp <j.charpentier@outlook.fr>
2022-11-22 13:57:11 +0100

    connect to backend

--- frontend/src/pages/Tab1.tsx
+++ frontend/src/pages/Tab1.tsx
@@ -1,16 +1,23 @@
-import {
-  IonContent,
-  IonPage,
-} from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 import "../components/styles/MonApp.css";
 import Navbar from "../components/Navbar.jsx";
 import { useRecoilState } from "recoil";
 import { titleSessionState } from "../recoil";
 import { Link } from "react-router-dom";
 import Tab5 from "./Tab5.jsx";
+import { useEffect } from "react";
+import axios from "axios";
 
 const Tab1: React.FC = () => {
   const [titleSession, setTitleSession] = useRecoilState(titleSessionState);
+
+  useEffect(() => {
+    axios.get("http://localhost:3001/exercises").then((res) => {
+      console.log("res", res);
+    });
+  }, []);
+
+  
   return (
     <IonPage>
       <IonContent fullscreen>
