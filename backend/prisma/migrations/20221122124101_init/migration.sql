06d4a1bc1b528caa7187a1617922236d5ed8ebd3
Julien Charp <j.charpentier@outlook.fr>
2022-11-22 13:42:27 +0100

    first models

---
+++ backend/prisma/migrations/20221122124101_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT
+);
+
+-- CreateTable
+CREATE TABLE "Exercise" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL
+);
