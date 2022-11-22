45887c970dc019b7a7295d179b2ba0fbc790f665
Julien Charp <j.charpentier@outlook.fr>
2022-11-22 13:51:51 +0100

    first route

--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
+app.get("/exercises", (req, res) => {
+  res.send([
+    { id: 1, name: "Exercise 1" },
+    { id: 2, name: "Exercise 2" },
+  ]);
+});
 
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
